c07636a451b17e57653cdb359ad6e7c1b43a9a55
brdeav39 <brideave@gmail.com>
2017-08-24 19:55:08 +0000

    Created a table that binds to an additional JSON model using Component.js and XML

--- helloworld/Component.js
+++ helloworld/Component.js
@@ -11,39 +11,36 @@ sap.ui.define([
 		init: function() {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
-			
+
 			// mock data models
 			var oDataRec = {
-				recipient : {
+				recipient: {
 					name: "World",
 					location: "Earth",
 					pos: "life"
 				}
 			};
-			var	oDataUsers = {
-				user1 : {
+			var oDataUsers = {
+				data: [{
 					name: "brdeav39",
 					id: "545789432",
 					pos: "analyst",
 					level: "5"
-				},
-				user2 : {
+				}, {
 					name: "ddd124",
 					id: "124586754",
 					pos: "supervisor",
 					level: "10"
-				},
-				user3 : {
+				}, {
 					name: "eee4324",
 					id: "674788909",
 					pos: "analyst 2",
 					level: "8"
-				}
+				}]
 			};
-			var oModelRec = new JSONModel(oDataRec);
-			var oModelUsers = new JSONModel(oDataUsers);
-			this.setModel(oModelRec, "oModelRec");
-			this.setModel(oModelUsers,  "oModelUsers");
+
+			this.setModel(new JSONModel(oDataRec), "oModelRec");
+			this.setModel(new JSONModel(oDataUsers), "oModelUsers");
 		}
 	});
 });
--- helloworld/controller/App.controller.js
+++ helloworld/controller/App.controller.js
@@ -6,15 +6,15 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("sap.ui.helloworld.namespace.controller.App", {
-		onShowHello : function () {
+		onShowHello: function() {
 			//this gives warning - doesn't meet Fiori guidelines apparently because oData is private. See subsequent code
 			// MessageToast.show("Hello, " + this.getView().getModel().oData.recipient.name);
-			
+
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel("oModelRec").getProperty("/recipient/name");
+			var sRecipient = this.getOwnerComponent().getModel("oModelRec").getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-			
+
 			//show message
 			MessageToast.show(sMsg);
 			//MessageToast.show("Hello, World");
